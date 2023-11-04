ba9849b20727d1c67dddb3d7e5fa13ea234a73bf
tatianacarcamoreula <134174569+tatianacarcamoreula@users.noreply.github.com>
2023-11-03 23:32:06 -0300

    Add files via upload

--- ejercicios_practica/marvel/e_commerce/api/urls.py
+++ ejercicios_practica/marvel/e_commerce/api/urls.py
@@ -3,9 +3,9 @@ from e_commerce.api.views import *
 
 
 urlpatterns = [
-    path('comic-list/', comic_list_api_view),
-    path('comic-retrieve/', comic_retrieve_api_view),
-    path('comic-create/', comic_create_api_view),
-    path('comic-list-filtered/', comic_list_filtered_api_view),
-    path('comic-filter-stock_qty/', comic_filter_stock_qty_api_view),
+    path('comic-list/', comic_list_api_view, name='comic_list_api_view'),
+    path('comic-retrieve/', comic_retrieve_api_view, name='comic_retrieve_api_view'),
+    path('comic-create/', comic_create_api_view, name= 'comic_create_api_view'),
+    path('comic-list-filtered/', comic_list_filtered_api_view, name='comic_list_filtered_api_view'),
+    path('comic-filter-stock_qty/', comic_filter_stock_qty_api_view, name= 'comic_filter_stock_qty_api_view'),
 ]
